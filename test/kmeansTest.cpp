d824d254287939a03a215b50cb5564cd44218986
richie <rgkirch@gmail.com>
2018-01-16 23:05:41 -0500

    failing test

--- test/kmeansTest.cpp
+++ test/kmeansTest.cpp
@@ -2,10 +2,9 @@
 #include "gmock/gmock.h"
 #include "kmeans.hpp"
 
-TEST(a,b) {
-    // Point<3> a {1,3,9};
-    // Point<3> b {0,4,12};
-    // auto result = minus(a,b);
-    // Point<3> expected {1, -1, -3};
-    // ASSERT_EQ(expected, result);
+TEST(thisPasses, helloHello) {
+    ASSERT_EQ("hello", "hello");
+}
+TEST(thisFails,helloWorld) {
+    ASSERT_EQ("hello", "world");
 }
