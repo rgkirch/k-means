84f2e87e44ecde52e9ed48759466e9c9cb0b8c0d
richie <rgkirch@gmail.com>
2018-01-28 12:58:09 -0500

    unpleasant

--- src/copyImage.hpp
+++ src/copyImage.hpp
@@ -12,18 +12,19 @@ int copyImage() {
   try {
     image.read("/mnt/c/Users/richie/Documents/github/k-means/"
                "resources/mnms.jpg");
-    image.modifyImage();
+    image.modifyImage(); // the internet says I need it but it worked without it
     auto width = image.columns();
     auto height = image.rows();
     PixelPacket *pixels = image.getPixels(0, 0, width, height);
 
-    double x = 65535;
+    double x = 65535; // unpleasant
     for (int i = 0; i < width * height; ++i) {
-      *pixels = ColorRGB(pixels->red / x, pixels->green / x, pixels->blue / x);
+      *pixels = ColorRGB(pixels->red / x, pixels->green / x,
+                         pixels->blue / x); // unpleasant
       pixels++;
     }
 
-    image.syncPixels();
+    image.syncPixels(); // the internet says I need it but it worked without it
     image.write("/mnt/c/Users/richie/Documents/github/k-means/"
                 "resources/copy.jpg");
   } catch (Exception &error_) {
